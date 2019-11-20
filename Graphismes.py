534736f476ed49c9bfb09434a796d191809d11bd
hugobetin <33903452+hugobetin@users.noreply.github.com>
2019-11-20 23:18:14 +0100

    Update Graphismes.py

--- Graphismes.py
+++ Graphismes.py
@@ -1,13 +1,39 @@
 import pygame
 import pygame.draw as dr
 import Donnees as dn
+from Font import font, tracer_message
 
 ##Afficher l'écran
 
 def afficher_ecran(screen,vision):
     x=dn.joueur[0]
     y=dn.joueur[1]
-    #On dessine d'abord le fond à l'aide de grille_vide.
+    #On dessine d'abord le fond d'écran.
+    draw_background(screen,vision)
+    
+    #On met ensuite le personnage au centre, et les ennemis à l'écran autour.
+    draw_charac(screen, vision, "joueur", 0.5*dn.taille_ecran, 0.5*dn.taille_ecran)
+    for e in dn.ennemis:
+        draw_charac(screen, vision, "ennemi", e[0][0]-(x-3)+0.5, e[0][1]-(y-3)+0.5)
+        #-(x/y-3) pour avoir la postion par rapport au bord de l'écran
+        #+0.5 pour être sur le centre d'une case, et non sur un coin
+        #la virgule n'est pas génante car draw_charac convertie en entier après le calcul
+    
+    #On dessine la carte.
+    draw_map(screen,vision)
+    
+    #On dessine le reste de l'UI.
+    draw_UI(screen)
+
+    #Enfin, on affiche l'écran actualisé.
+    pygame.display.flip()
+    
+
+
+def draw_background(screen,vision):
+    #Cette fonction parcours grille_vide autour de la position du joueur, et dessine la case appropriée.
+    x=dn.joueur[0]
+    y=dn.joueur[1]
     for i in range(dn.taille_ecran):
         for j in range(dn.taille_ecran):
             if dn.grille_vide[x-3+i,y-3+j]==3:
@@ -16,13 +42,15 @@ def afficher_ecran(screen,vision):
                 dr.rect(screen, dn.palette[0],[j*dn.taille_case, i*dn.taille_case, dn.taille_case,dn.taille_case])
             else:
                 dr.rect(screen, dn.palette[1],[j*dn.taille_case, i*dn.taille_case, dn.taille_case,dn.taille_case])
-    #On met ensuite le personnage au centre.
-    dr.circle(screen, dn.YELLOW, [int(0.5*dn.taille_ecran*dn.taille_case), int(0.5*dn.taille_ecran*dn.taille_case)],int(0.5*dn.taille_case))
-    #Puis on dessine les ennemis.
-    for e in dn.ennemis:
-        dr.circle(screen, dn.RED, [int((e[0][1]-y+3+0.5)*dn.taille_case), int((e[0][0]-x+3+0.5)*dn.taille_case)],int(0.5*dn.taille_case))
-    
-    #Enfin, on dessine la carte.
+
+
+def draw_charac(screen, vision, charac, x, y):
+    dr.circle(screen, dn.sprites[charac], [int(y*dn.taille_case), int(x*dn.taille_case)], int(0.5*dn.taille_case))
+
+
+def draw_map(screen, vision):
+    x=dn.joueur[0]
+    y=dn.joueur[1]
     for i in range(dn.width_map+1):
         for j in range(dn.height_map+1):
             #Pour chaque case, on vérifie s'il y a un escalier. S'il y en a un, on le marque d'un carré bleu ciel
@@ -41,6 +69,6 @@ def afficher_ecran(screen,vision):
     for e in dn.ennemis:
         if e[0][0]<=vision[1] and e[0][0]>=vision[0] and e[0][1]<=vision[3] and e[0][1]>=vision[2]:
             dr.circle(screen, dn.RED, [int(25+(e[0][1]-3+0.5)*5)+1, int(25+(e[0][0]-3+0.5)*5)+1],2)
-    
-    #On affiche l'écran actualisé.
-    pygame.display.flip()
+
+def draw_UI(screen):
+    tracer_message(screen,'E.'+str(dn.etage),2,dn.taille_ecran*dn.taille_case-35)
